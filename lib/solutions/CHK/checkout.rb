b23947e00ddda46b355b0f351ff1462d8dfc932d
sbx_user1051 <sbx_user1051@169.254.160.173>
2019-04-09 10:16:10 +0000

    Tue Apr 09 10:16:10 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,9 +1,18 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
-
+  PRICES = {A: 50, B: 30, C: 20, D: 15}
+ #  A    | 50    | 3A for 130     |
+ # | B    | 30    | 2B for 45      |
+ # | C    | 20    |                |
+ # | D    | 15
   def checkout(skus)
-    skus.split(",")
+    total_price = 0
+    skus.split(",").each |item| do
+      
+    end
+
   end
 
 end
+
 
