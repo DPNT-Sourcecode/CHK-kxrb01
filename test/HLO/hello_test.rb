61b7d386cde00f13470b11a2e7e448da9a740752
sbx_user1051 <sbx_user1051@169.254.160.173>
2019-04-09 09:52:31 +0000

    Tue Apr 09 09:52:31 UTC 2019

--- test/HLO/hello_test.rb
+++ test/HLO/hello_test.rb
@@ -7,8 +7,6 @@ require_solution 'HLO'
 
 class ClientTest < Minitest::Test
   def test_hello
-    assert_equal "Hello, World!", Hello.new.hello("Friend"), 'Should greet the friend'
+    assert_equal "Hello, John!", Hello.new.hello("John"), 'Should greet the friend'
   end
 end
-
-
