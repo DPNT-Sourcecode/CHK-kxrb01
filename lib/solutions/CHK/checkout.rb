ec30de497d1b477af77ea622b68b59a851cdbc81
sbx_user1051 <sbx_user1051@169.254.41.189>
2019-04-10 12:04:33 +0000

    Wed Apr 10 12:04:33 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,9 +1,42 @@
 # noinspection RubyUnusedLocalVariable
+# | G    | 20    |                        |
+# | H    | 10    | 5H for 45, 10H for 80  |
+# | I    | 35    |                        |
+# | J    | 60    |                        |
+# | K    | 80    | 2K for 150             |
+# | L    | 90    |                        |
+# | M    | 15    |                        |
+# | N    | 40    | 3N get one M free      |
+# | O    | 10    |                        |
+# | P    | 50    | 5P for 200             |
+# | Q    | 30    | 3Q for 80              |
+# | R    | 50    | 3R get one Q free      |
+# | S    | 30    |                        |
+# | T    | 20    |                        |
+# | U    | 40    | 3U get one U free      |
+# | V    | 50    | 2V for 90, 3V for 130  |
+# | W    | 20    |                        |
+# | X    | 90    |                        |
+# | Y    | 10    |                        |
+# | Z    | 50    |                        |
+# +------+-------+------------------------+
+
 class Checkout
   ITEMS = ['A', 'B', 'C', 'D', 'E', 'F']
   PRICES = [{item: 'A', count: 1, price: 50}, {item: 'B', count: 1, price: 30},
             {item: 'C', count: 1, price: 20}, {item: 'D', count: 1, price: 15},
-            {item: 'E', count: 1, price: 40}, {item: 'F', count: 1, price: 10}]
+            {item: 'E', count: 1, price: 40}, {item: 'F', count: 1, price: 10},
+            {item: 'G', count: 1, price: 20}, {item: 'H', count: 1, price: 10},
+            {item: 'I', count: 1, price: 35}, {item: 'J', count: 1, price: 60},
+            {item: 'K', count: 1, price: 80}, {item: 'L', count: 1, price: 90},
+            {item: 'M', count: 1, price: 15}, {item: 'N', count: 1, price: 40},
+            {item: 'O', count: 1, price: 10}, {item: 'P', count: 1, price: 50},
+            {item: 'Q', count: 1, price: 30}, {item: 'R', count: 1, price: 50},
+            {item: 'S', count: 1, price: 30}, {item: 'T', count: 1, price: 20},
+            {item: 'G', count: 1, price: 20}, {item: 'H', count: 1, price: 10},
+            {item: 'G', count: 1, price: 20}, {item: 'H', count: 1, price: 10},
+            {item: 'G', count: 1, price: 20}, {item: 'H', count: 1, price: 10},
+            ]
   MULTIBUY_OFFERS = [{item: 'A', count: 3, offer_price: 130},
                      {item: 'B', count: 2, offer_price: 45},
                      {item: 'A', count: 5, offer_price: 200},
@@ -79,3 +112,4 @@ class Checkout
     end
   end
 end
+
