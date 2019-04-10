044c8313f979a436c29cfb99983e84d5a37464d6
sbx_user1051 <sbx_user1051@169.254.41.189>
2019-04-10 14:08:42 +0000

    Wed Apr 10 14:08:42 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -132,14 +132,9 @@ class Checkout
       total_qty_to_reduce = number_of_group_discount_offers * GROUP_DISCOUNT_OFFER[:count]
       p sorted_discount_basket_items = discount_basket_items.sort_by{|a_discount_item| a_discount_item[:price]}.reverse
       sorted_discount_basket_items.each do |sorted_basket_item|
+        if sorted_basket_item[:count] >= GROUP_DISCOUNT_OFFER[:count]
+        end
       end
     end
   end
 end
-
-
-
-
-
-
-
--- test/CHK/checkout_test.rb
+++ test/CHK/checkout_test.rb
@@ -199,5 +199,3 @@ class ClientTest < Minitest::Test
   end
 
 end
-
-
